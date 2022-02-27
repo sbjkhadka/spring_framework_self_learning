4cb25296827906ab14512bc7a6775b0a733f44db
subarna <khadka.subarnabijaya@gmail.com>
2022-02-27 17:38:12 -0500

    Assignment 2 Adding list to constructor arg

--- src/main/java/com/springcore/ci/Person.java
+++ src/main/java/com/springcore/ci/Person.java
@@ -1,18 +1,22 @@
 package com.springcore.ci;
 
+import java.util.List;
+
 public class Person {
 	private String name;
 	private int personId;
 	private Certifiacate certificate;
+	private List<String> phones;
 	
-	public Person(String name, int personId, Certifiacate certificate) {
+	public Person(String name, int personId, Certifiacate certificate,List<String> phones) {
 		this.name = name;
 		this.personId = personId;
 		this.certificate =certificate;
+		this.phones=phones;
 	}
 
 	@Override
 	public String toString() {
-		return this.personId+""+" : "+this.name+"{"+this.certificate+"}";
+		return this.personId+""+" : "+this.name+"{"+this.certificate+"}"+this.phones.get(0);
 	}
 }
